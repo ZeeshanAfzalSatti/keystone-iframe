e4dbbc9effd2b61b8f162635a5c7ca205e646e09
ZeeshanAfzalSatti <zeeshanqau2013@gmail.com>
2018-12-16 18:12:22 +0500

    Fix Structure

--- admin/client/App/components/Navigation/Primary/index.js
+++ admin/client/App/components/Navigation/Primary/index.js
@@ -37,13 +37,13 @@ var PrimaryNavigation = React.createClass({
 
 		return (
 			<PrimaryNavItem
-				label="glyphicon glyphicon-log"
-				href={this.props.signoutUrl}
-				title="Sign Out"
+		label="glyphicon glyphicon-log"
+		href={this.props.signoutUrl}
+		title="Sign Out"
 			>
-				 Sign Out
-			</PrimaryNavItem>
-		);
+			Sign Out
+		</PrimaryNavItem>
+	);
 	},
 	// Render the back button
 	renderBackButton () {
@@ -51,22 +51,22 @@ var PrimaryNavigation = React.createClass({
 
 		return (
 			<PrimaryNavItem
-				label="octicon-globe"
-				href={Keystone.backUrl}
-				title={'Front page - ' + this.props.brand}
+		label="octicon-globe"
+		href={Keystone.backUrl}
+		title={'Front page - ' + this.props.brand}
 			>
-				<span className="octicon octicon-globe" />
+			<span className="octicon octicon-globe" />
 			</PrimaryNavItem>
-		);
+	);
 	},
 	// Render the link to the webpage
 	renderFrontLink () {
 		return (
 			<ul className="app-nav app-nav--primary app-nav--right">
-				{/*this.renderBackButton()*/}
-				{this.renderSignout()}
-			</ul>
-		);
+			{/*this.renderBackButton()*/}
+		{this.renderSignout()}
+	</ul>
+	);
 	},
 	renderBrand () {
 		// TODO: support navbarLogo from keystone config
@@ -75,51 +75,56 @@ var PrimaryNavigation = React.createClass({
 		const { brand, currentSectionKey } = this.props;
 		const className = currentSectionKey === 'dashboard' ? 'primary-navbar__brand primary-navbar__item--active' : 'primary-navbar__brand';
 		return (
-		 	<PrimaryNavItem
-				className={className}
-				href={Keystone.backUrl}
-				label="octicon-home"
-				title={'Front page - ' + this.props.brand}
-		 	>
-		 		Home
-		 	</PrimaryNavItem>
-	 	);
+			<PrimaryNavItem
+		className={className}
+		href={Keystone.backUrl}
+		label="octicon-home"
+		title={'Front page - ' + this.props.brand}
+			>
+			Home
+			</PrimaryNavItem>
+	);
 	},
 	// Render the navigation
 	renderNavigation () {
 		if (!this.props.sections || !this.props.sections.length) return null;
 
-		return this.props.sections.map((section) => {
+		let array =  this.props.sections.map((section) => {
 			// Get the link and the class name
 			const href = section.lists[0].external ? section.lists[0].path : `${Keystone.adminPath}/${section.lists[0].path}`;
 			const isActive = this.props.currentSectionKey && this.props.currentSectionKey === section.key;
 			const className = isActive ? 'primary-navbar__item--active' : null;
-
 			return (
 				<PrimaryNavItem
-					active={isActive}
-					key={section.key}
-					label={section.label}
-					className={className}
-					to={href}
+			active={isActive}
+			key={section.key}
+			label={section.label}
+			className={className}
+			to={href}
 				>
-					{section.label}
-				</PrimaryNavItem>
-			);
+				{section.label}
+		</PrimaryNavItem>
+		);
 		});
+
+		array.push(<li className="primary-navbar__item">
+			<a className='primary-navbar__link' href="http://www.google.com" target='_blank'> Reports and Receipts </a>
+		</li>)
+
+		return array
 	},
 	render () {
 		if (!this.state.navIsVisible) return null;
 
 		return (
 			<nav className="primary-navbar">
-				<ul className="app-nav app-nav--primary app-nav--left">
-					{this.renderBrand()}
-					{this.renderNavigation()}
-				</ul>
-				{this.renderFrontLink()}
-			</nav>
-		);
+			<ul className="app-nav app-nav--primary app-nav--left">
+			{this.renderBrand()}
+		{this.renderNavigation()}
+	</ul>
+		{this.renderFrontLink()}
+	</nav>
+	);
 	},
 });
 
